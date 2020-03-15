703ec680fb12bb9d6f817c2ae3ece01e5e89a5ba
Darryl Bartlett <52551113+BUSHPUNK@users.noreply.github.com>
2020-03-16 09:08:58 +1000

    Add files via upload

--- examples/BushpunkRobot_servoExample.ino
+++ examples/BushpunkRobot_servoExample.ino
@@ -1,16 +1,26 @@
+/* BushpunkRobot_servoExample
+ *
+ * Written by: Darryl Bartlett
+ *             bushpunk.art
+ * 
+ * Scenerio:   A robot dog that has its tail attached directly to a servo.
+ *             The dog's tail wags constantly back and forth.
+ */
+
+
 #include <BushpunkRobot.h>
 
-BushpunkRobot robot;				// Create a BushpunkRobot object, called robot
-thingy headServo;				// Create a thingy, called headServo
+BushpunkRobot robot;                            // Create a BushpunkRobot object, called robot
+thingy tailServo;                               // Create a thingy, called tailServo
 
 setup() {
-    headServo = robot.addThingy("servo", 4);	// Add a servo that's connected to pin 4
-    robot.setServoSpeed(headServo, 2);		// Set the servos default speed to 2 (range = 1 to 5)
+    tailServo = robot.addThingy("servo", 4);    // Add a servo that's connected to pin 4
+    robot.setServoSpeed(tailServo, 2);          // Set the servos default speed to 2 (range = 1 to 5)
 }
 
 main() {
-    robot.moveServo(headServo, 45);		// Move the servo to 45 degrees
-    robot.waitForServo(headServo);		// Wait until the servo has reached its set position
-    robot.moveServo(headServo, 135);		// Move the servo to 135 degrees
-    robot.waitForServo(headServo);		// Wait until the servo has reached its set position
+    robot.moveServo(tailServo, 45);             // Move the servo to 45 degrees
+    robot.waitForServo(tailServo);              // Wait until the servo has reached its set position
+    robot.moveServo(tailServo, 135);            // Move the servo to 135 degrees
+    robot.waitForServo(tailServo);              // Wait until the servo has reached its set position
 }
